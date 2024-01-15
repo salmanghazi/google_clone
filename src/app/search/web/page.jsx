c9c8214cba79c6b33e34d4dec20ba807ae2a79bd
msalmanKT <muhammad.salman@gomotive.com>
2024-01-16 00:24:38 +0500

    Added google search API in the project

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export default function page() {
-  return (
-    <div>web page</div>
-  )
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(`
+    https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}
+  `);
+
+  const data = await response.json();
+  const { items } = data;
+  
+  return <>
+    {items?.map((item, index) => (
+      <div key={index}>
+        <h1>{item.title}</h1>
+      </div>
+    ))}
+  </>;
 }
